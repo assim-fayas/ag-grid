69b3a3f1dca96026591bcc77e9b6b9ffe51a87cb
assim fayas <asim1fayas@gmail.com>
2024-05-12 21:43:13 +0530

    basic table formate implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import{HttpClient}from '@angular/common/http';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'ag-grid';
+
+http:HttpClient=inject(HttpClient)
+
+tableData:any[]=[]
+
+
+ngOnInit(): void {
+  this.http.get<any>('https://jsonplaceholder.typicode.com/users').subscribe((data)=>{
+    console.log(data);
+    
+this.tableData=data
+  })
+}
+
+colDefs: ColDef[] = [
+  { field: "id",flex:0, },
+  { field: "name",flex:2  },
+  { field: "username",flex:2 },
+  { field: "email" ,flex:2},
+  { field: "website" ,flex:2},
+ 
+];
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AgGridAngular } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AgGridAngular
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
